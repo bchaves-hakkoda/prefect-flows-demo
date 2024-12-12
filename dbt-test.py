98d9b468afd3aec1323d38027067af4da37ee324
Betzabet Chaves <betzabet_chaves@hakkoda.io>
2024-12-12 08:43:54 -0600

    test

--- dbt-test.py
+++ dbt-test.py
@@ -4,7 +4,7 @@ from prefect_dbt.cloud.jobs import run_dbt_cloud_job
 
 
 @flow
-def flow():
+def flow_dbt():
     dbt_cloud_credentials = DbtCloudCredentials.load("NESTLE snowflake")
     dbt_cloud_job = DbtCloudJob(
         dbt_cloud_credentials=dbt_cloud_credentials, job_id=776963
