388d39fbd04be3df7eb3a0bec76c41ed7da3d41b
Betzabet Chaves <betzabet_chaves@hakkoda.io>
2024-12-12 10:44:07 -0600

    test

--- dbt-test.py
+++ dbt-test.py
@@ -4,9 +4,9 @@ from prefect_dbt.cloud.jobs import run_dbt_cloud_job
 
 
 @flow
-def flow_dbt():
+def flow_dbt(job_id: int):
     dbt_cloud_credentials = DbtCloudCredentials.load("by-connection")
     dbt_cloud_job = DbtCloudJob(
-        dbt_cloud_credentials=dbt_cloud_credentials, job_id=776963
+        dbt_cloud_credentials=dbt_cloud_credentials, job_id=job_id
     )
     return run_dbt_cloud_job(dbt_cloud_job=dbt_cloud_job)
